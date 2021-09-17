658c1503e9088962714f9391f15da5511aba610d
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2021-09-17 00:43:17 -0500

    get api key added

---
+++ lib/get_api_key.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:universal_platform/universal_platform.dart';
+
+Future<String> get_api_key({required String name}) async {
+  DocumentSnapshot gcp =
+      await FirebaseFirestore.instance.collection("metadata").doc("gcp").get();
+  Map<String, dynamic> gcp_data = gcp.data() as Map<String, dynamic>;
+  String api_key = "";
+
+  if (UniversalPlatform.isAndroid) {
+    api_key = gcp_data["keys"][name]["android"];
+  } else if (UniversalPlatform.isIOS) {
+    api_key = gcp_data["keys"][name]["ios"];
+  } else if (UniversalPlatform.isWeb) {
+    api_key = gcp_data["keys"][name]["web"];
+  }
+
+  return api_key;
+}
