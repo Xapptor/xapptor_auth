d85f8d74c0c4eab94e78fb6babe8e5852c574742
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2023-10-04 16:16:44 -0600

    Fix: validation added for FirebaseAuth.instance.currentUser in user_id_button widget

--- lib/account_view/user_id_button.dart
+++ lib/account_view/user_id_button.dart
@@ -6,48 +6,50 @@ import 'package:xapptor_ui/values/ui.dart';
 
 extension UserIDButton on AccountViewState {
   Widget user_id_button() {
-    User user = FirebaseAuth.instance.currentUser!;
+    User? user = FirebaseAuth.instance.currentUser;
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          flex: 10,
-          child: Container(
-            margin: EdgeInsets.only(bottom: sized_box_space),
-            child: TextButton(
-              onPressed: () {
-                copy_to_clipboard(
-                  data: user.uid,
-                  message: "User ID copied to clipboard",
-                  context: context,
-                );
-              },
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(
-                  Colors.grey.withOpacity(0.3),
-                ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(outline_border_radius),
+    return user == null
+        ? Container()
+        : Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 10,
+                child: Container(
+                  margin: EdgeInsets.only(bottom: sized_box_space),
+                  child: TextButton(
+                    onPressed: () {
+                      copy_to_clipboard(
+                        data: user.uid,
+                        message: "User ID copied to clipboard",
+                        context: context,
+                      );
+                    },
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                        Colors.grey.withOpacity(0.3),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(outline_border_radius),
+                          ),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      "User ID: ${user.uid}",
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
                     ),
                   ),
                 ),
               ),
-              child: Text(
-                "User ID: ${user.uid}",
-                textAlign: TextAlign.start,
-                style: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 12,
-                ),
-              ),
-            ),
-          ),
-        ),
-        const Spacer(flex: 1),
-      ],
-    );
+              const Spacer(flex: 1),
+            ],
+          );
   }
 }
